09d134a480e858160064559c7870c1d887cd3d97
Naama Carmel <naama271@gmail.com>
2021-05-11 16:56:24 +0300

    prod

--- client/src/API/api.js
+++ client/src/API/api.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === "development") {
 	origin = "http://localhost:5000/api/";
 }
 if (process.env.NODE_ENV === "production") {
-	origin = "/api";
+	origin = "https://haakaaisreal.herokuapp.com/api/";
 }
 
 const api = axios.create({
