cf752b2f364c18ba4e3bdde9d6417473ac2136aa
Naama Carmel <naama271@gmail.com>
2021-05-18 08:57:52 +0300

    admin and updated routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,40 @@
 //import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header/Header';
-import About from './components/About';
-import Products from './components/Products/Products';
-import Distributors from './components/Distributors/Distributors';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import {Helmet} from "react-helmet";
+import "./App.css";
+import Header from "./components/Header/Header";
+import About from "./components/About";
+import Products from "./components/Products/Products";
+import Distributors from "./components/Distributors/Distributors";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import { Helmet } from "react-helmet";
 import NotFound from "./components/NotFound";
-import Product from './components/Products/Product';
-import {BrowserRouter , Route, Switch, HashRouter} from 'react-router-dom'
-import Home from './components/Home';
-import Login from './components/admin/Login';
-import admin from './components/admin/Admin';
-
+import Product from "./components/Products/Product";
+import { BrowserRouter, Route, Switch, HashRouter } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/admin/Login";
+import admin from "./components/admin/Admin";
 
 function App() {
-
   return (
     <div>
-      <BrowserRouter>
-      <Header /> 
-      <Helmet>
-        <title>Haakaa Israel</title>
-        <meta name="Haakaa Israel" content="Haakaa Israel" />
+      <HashRouter>
+        <Header />
+        <Helmet>
+          <title>Haakaa Israel</title>
+          <meta name="Haakaa Israel" content="Haakaa Israel" />
         </Helmet>
-   <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/#about" component={About} />
-      <Route exact path="/#distributors" component={Distributors} />
-      <Route exact path="/#contact" component={Contact} />
-      <Route exact path="/#products" component={Products} />
-      <Route exact path="/products/:id" component={Product} />
-      <Route exact path="/admin" component={Login} />
-      <Route component={NotFound} />
-      </Switch >
-     
-  </BrowserRouter>
-  <Footer />
-  
-    
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/distributors" component={Distributors} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/products" component={Products} />
+          <Route exact path="/products/:id" component={Product} />
+          <Route exact path="/admin" component={Login} />
+          <Route component={NotFound} />
+        </Switch>
+      </HashRouter>
+      <Footer />
     </div>
   );
 }
--- client/src/components/Header/Navbarlib.js
+++ client/src/components/Header/Navbarlib.js
@@ -6,7 +6,7 @@ import React from "react";
 
 import logoimage from './images/Israelogo150.png'
 import { HashLink } from 'react-router-hash-link';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter , Link} from 'react-router-dom';
 
 //<HashLink to="/#distributors"></HashLink>
 
@@ -41,17 +41,16 @@ function Example() {
     //   ]}
     // />
     
-    <BrowserRouter>
-   
+  <>
    <HashLink to='/#home'><img src={logoimage} className="logo" alt="Haakaa logo"/></HashLink>
     
       <ul>
-        <li><HashLink to='/#about'>הסיפור שלנו</HashLink></li>
-        <li><HashLink to="/#products">המוצרים שלנו</HashLink></li>
-        <li><HashLink to="/#distributors">נקודות ההפצה</HashLink></li>
-        <li><HashLink to="/#contact">יצירת קשר</HashLink></li>
+        <HashLink to='/#about'>הסיפור שלנו</HashLink>
+        <HashLink to="/#products">המוצרים שלנו</HashLink>
+        <HashLink to="/#distributors">נקודות ההפצה</HashLink>
+        <HashLink to="/#contact">יצירת קשר</HashLink>
       </ul>
-    </BrowserRouter>
+</>
     );
 
 };
