1aac017925a56cc96ace38e4c09f9f7ad18c9706
Naama Carmel <naama271@gmail.com>
2021-05-19 13:46:53 +0300

    form

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -15,15 +15,26 @@ let userID = 'user_JEZO1HoxX0qv5PhTYQDdb'
 const ContactForm = () => {
   const [toSend, sendForm] = useState({
     from_name: '',
-    to_name: '',
+    phone: '',
     message: '',
     reply_to: '',
   });
 
   const onSubmit = (e) => {
     e.preventDefault();
+    console.log(e.target.from_name.value)
+    console.log(e.target.phone.value)
+    console.log(e.target)
 
-    emailjs.sendForm('service_bgb0wad', 'template_2828nkq', e.target, 'user_JEZO1HoxX0qv5PhTYQDdb')
+    const phone = e.target.phone.value
+  
+      const regex = /^(?:(?:(\+?972|\(\+?972\)|\+?\(972\))(?:\s|\.|-)?([1-9]\d?))|(0[23489]{1})|(0[57]{1}[0-9]))(?:\s|\.|-)?([^0\D]{1}\d{2}(?:\s|\.|-)?\d{4})$/;
+      if (!phone.match(regex)) {
+        alert("מספר הנייד לא תקין... נסי שוב");
+      }
+   
+
+    emailjs.sendForm('service_bgb0wad', 'template_2828nkq', 'e.target', 'user_JEZO1HoxX0qv5PhTYQDdb')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
@@ -42,32 +53,31 @@ const ContactForm = () => {
   <input
     type='text'
     name='from_name'
-    placeholder='from name'
+    placeholder='השם שלך'
     value={toSend.from_name}
     onChange={handleChange}
   />
   <input
-    type='text'
-    name='to_name'
-    placeholder='to name'
-    value={toSend.to_name}
+    type='number'
+    name='phone'
+    placeholder='מספר הנייד שלך'
+    value={toSend.phone}
     onChange={handleChange}
   />
-  <input
-    type='text'
+  <textarea
     name='message'
-    placeholder='Your message'
+    placeholder='מה שתרצי לשאול או להגיד לנו'
     value={toSend.message}
     onChange={handleChange}
   />
   <input
-    type='text'
+    type='email'
     name='reply_to'
-    placeholder='Your email'
+    placeholder='המייל שלך'
     value={toSend.reply_to}
     onChange={handleChange}
   />
-  <button type='submit'>Submit</button>
+  <button type='submit'>דברו איתי</button>
 </form>
     </>
   );
